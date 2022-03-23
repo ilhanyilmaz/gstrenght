5c742a25270afc95557ba722ca3616962ecde6f4
ilhanylmaz1@gmail.com <Mdbf5911>
2022-03-23 19:15:21 +0300

    version 2.0

--- code/read_and_plot.py
+++ code/read_and_plot.py
@@ -1,25 +1,58 @@
 # encoding:utf-8
 #https://pyserial.readthedocs.io/en/latest/shortintro.html#eol
-
+import sys
 import serial
 from serial import Serial
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
+from matplotlib.widgets import Button, TextBox
+import matplotlib.text as text
 
 import csv
 import time
 from itertools import count
 import threading
 from tools.ports import select_port
-from tools.settings import PORT_NAME, BAUD_RATE, TIMESTAMP_FORMAT, MIN_FORCE
+from tools.settings import DEBUG, PORT_NAME, BAUD_RATE, TIMESTAMP_FORMAT, MIN_FORCE, PLOT_STYLE, X_LABEL, Y_LABEL
 from datetime import datetime
 
-data_name = input("Isim girin: ")
+reading_active = False
+current_reading_starting_index = 0
+data_name = "default"
 x_vals = []
 y_vals = []
+try_maxes =[]
+
 index = count()
+
+ser = None
 read_data = True
+x = None
+y = None
+fig, ax=plt.subplots()
+fig.subplots_adjust(bottom=0.2)
+text_box = None
+axbox = fig.add_axes([0.1, 0.01, 0.3, 0.05])
+text_box = TextBox(axbox, 'Isim', textalignment='center')
+buttonbox = fig.add_axes([0.50, 0.01, 0.14, 0.05])
+btn_stop = Button(buttonbox, 'Kaydet')
+buttonbox2 = fig.add_axes([0.65, 0.01, 0.14, 0.05])
+btn_new = Button(buttonbox2, 'Yeni Kayıt')
+buttonbox3 = fig.add_axes([0.80, 0.01, 0.14, 0.05])
+btn_clear = Button(buttonbox3, 'Temizle')
+
+#btn_stop = Button(buttonbox, 'Kaydet')
+
+warning = ""
+
+def reset_values():
+    global x_vals, y_vals, try_maxes,current_reading_starting_index
+    x_vals=[]
+    y_vals=[]
+    try_maxes=[]
+    current_reading_starting_index=0
+    #input_name()
 
 def get_connection():
     com_port = PORT_NAME
@@ -31,28 +64,31 @@ def get_connection():
             ser.open()
             return ser
         except:
-            com_port = select_port()
+            com_port = select_port()    
 
 def get_timestamp():
     return datetime.now().strftime(TIMESTAMP_FORMAT)
+
 def save_graph(timestamp=None):
     if timestamp == None:
         timestamp = get_timestamp()
-    plt.savefig("../graph/" + data_name + timestamp + '.png')
+    plt.savefig("../graph/" + data_name + "_" + timestamp + '.png')
 
 def plot_graph(x_values, y_values):
-    plt.cla()
+    ax.cla()
+    plt.style.use(PLOT_STYLE)
     legend = data_name
     if len(y_values)>0:
          legend = legend+ "(Max: " + str(max(y_values)) +")"
-    plt.plot(x_values, y_values, label=legend)
-    
-    plt.legend(loc='upper center')
-    plt.tight_layout()
+    ax.plot(x_values, y_values, label=legend)
+    ax.set_ylabel(Y_LABEL)
+    ax.set_xlabel(X_LABEL + '\n ' + warning)
+    ax.legend(loc='upper center')
+    #plt.tight_layout()
     plt.show()
 
 def load_from_file():
-    data = pd.read_csv("../data/"+data_name + '.csv')
+    data = pd.read_csv("../data/" + data_name + '.csv')
     print("Max: " + str(max(data['y_value'])) )
     plot_graph(data['x_value'],data['y_value'])
     
@@ -60,7 +96,7 @@ def write_to_file(timestamp=None):
     if timestamp == None:
         timestamp = get_timestamp()
     fieldnames = ['x_value', 'y_value']
-    with open('../data/' + data_name + timestamp + '.csv', 'w') as csv_file:
+    with open('../data/' + data_name + "_" + timestamp + '.csv', 'w') as csv_file:
         csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
         csv_writer.writeheader()
 
@@ -72,16 +108,31 @@ def write_to_file(timestamp=None):
             }
 
             csv_writer.writerow(info)
-            
+def log_current_reading():
+    global reading_active, current_reading_starting_index, warning, try_maxes
+    reading_active= False
+    current_max = max(y_vals[current_reading_starting_index:])
+    try_maxes.append(current_max)
+    warning = str(try_maxes)
+    #warning = "current max: " + str(current_max)
+    current_reading_starting_index=len(x_vals)
+
 def get_data():
+    global ser, reading_active
     while read_data:
-        data = ser.readline(20) # try empty or different values
         try:
+            data = ser.readline(20) # try empty or different values
             val = float(data)
             if val < MIN_FORCE:
+                if reading_active:
+                    log_current_reading()
                 continue
+            elif reading_active == False:
+                reading_active = True
             x_vals.append(next(index)/40.0)
             y_vals.append(float(data))
+        except TypeError as e:
+            break
         except:
             data_str = str(data)
             pos = data_str.find('Max kg')
@@ -91,72 +142,112 @@ def get_data():
                 return
             else:
                 print(data)
-        
 
-def print_commands():
-    print(" ")
-    print("Komutlar:")
-    print(" ")
-    print("  [q] : Veri okumaya basla")
-    print("  [w] : Veri okumayi durdur")
-    print("  [s] : Verileri kaydet")
-    print("  [l] : Verileri goster")
-    print("  [g] : Grafik kaydet")
-    print("  [x] : Cikis")
-    print(" ")
+def start_reading():
+    global read_data, y
+    print("start_reading event triggered")
+    read_data=True
 
+    try:
+        ser.write(b'q')
+        y = threading.Thread(target=get_data)
+        y.start()
+    except:
+        print("Couldn't send command to device")
 
-def get_input():
+def stop_reading():
+    global read_data
+    print("stop_reading event triggered")
+    read_data=False
+    try:
+        ser.write(b'w')
+    except:
+        print("Couldn't send command to device")
+
+def name_change(expression):
+    global data_name, cid
+    print("name_change event triggered")
+    if expression:
+        data_name = expression
+        #text_box.on_submit(None)
+        axbox.set_visible(False)
+        buttonbox.set_visible(True)
+        buttonbox2.set_visible(True)
+        buttonbox3.set_visible(True)
+        #reset_values()
+        start_reading()
+
+def add_textbox():
+    axbox.set_visible(True)
+    buttonbox.set_visible(False)
+    buttonbox2.set_visible(False)
+    buttonbox3.set_visible(False)
+
+def on_clear(event):
+    reset_values()
+
+def on_stop_reading(event):
+    global warning
+    #print(str(event))
+    stop_reading()
     timestamp = get_timestamp()
+    write_to_file(timestamp)
+    save_graph(timestamp)
+    warning = "Grafik kaydedildi!"
 
-    while True:
-        print_commands()
-        val = input("Komut girin: ")
-        print(val)
-        if val == 'q':
-            read_data=True
-            try:
-                ser.write(b'q')
-                y = threading.Thread(target=get_data)
-                y.start()
-            except:
-                pass
+def on_new_reading(event):
+    global warning
+    #print(str(event))
+    #reset_values()
+    add_textbox()
 
-        elif val == 'w':
-            read_data=False
-            timestamp = get_timestamp()
-            try:
-                ser.write(b'w')
-            except:
-                pass
-        elif val == 's':
-            write_to_file(timestamp)
-        elif val == 'l':
-            read_data = False
-            load_from_file()
-        elif val == 'g':
-            save_graph(timestamp)
-        elif val == 'x':
-            plt.close()
-            try:
-                ser.close()
-            except:
-                pass
-            break
-        else:
-            print('Komut bulunamadi: ' + val)
+def on_close(event):
+    global read_data
+    read_data = False
+    #plt.close()
+    try:
+        ser.close()
+    except:
+        pass
 
 def animate(i):
     plot_graph(x_vals, y_vals)
-    
 
-ser = get_connection()
+def debug_animate(i):
+    data = pd.read_csv('data.csv')
+    x = data['x_value']
+    y1 = data['y_value']
 
-x = threading.Thread(target=get_input)
-x.start()
+    #x_vals.append(next(index))
+    #y_vals.append(random.randint(0, 5))
 
-ani = FuncAnimation(plt.gcf(), animate, interval=100) #500ms
-plt.tight_layout()
+    plt.cla()
+    plt.style.use(PLOT_STYLE)
+    #plt.plot(x_vals, y_vals, label='Maximum')
+    plt.plot(x, y1, label=str(data_name))
+    plt.annotate('100', (5, 5), (5, 1), ha='center', va='center', arrowprops = {'arrowstyle':"->", 'color':'C1'})
+    plt.legend(loc='upper center')
+    plt.tight_layout()
+
+#x = threading.Thread(target=get_input)
+#x.start()
+
+if DEBUG:
+    ani = FuncAnimation(plt.gcf(), debug_animate, interval=100) #500ms
+
+else:
+    ser = get_connection()
+    ani = FuncAnimation(plt.gcf(), animate, interval=100) #500ms
+
+
+btn_stop.on_clicked(on_stop_reading)
+btn_new.on_clicked(on_new_reading)
+btn_clear.on_clicked(on_clear)
+text_box.on_submit(name_change)
+add_textbox()
+
+fig.canvas.mpl_connect('close_event', on_close)
+#plt.tight_layout()
 plt.show()
 
 
--- code/tools/settings.py
+++ code/tools/settings.py
@@ -1,4 +1,8 @@
+DEBUG = False
 PORT_NAME = 'COM8'
 BAUD_RATE = 200
 TIMESTAMP_FORMAT = "%Y%m%d%H%M%S_"
 MIN_FORCE = 1.0
+PLOT_STYLE = 'bmh' # https://matplotlib.org/cheatsheets/_images/cheatsheets-1.png
+X_LABEL = 'zaman (s)'
+Y_LABEL = 'kuvvet (kN)'
